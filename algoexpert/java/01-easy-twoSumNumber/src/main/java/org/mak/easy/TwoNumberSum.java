f84e1b2d9674723b1b9e19d76e45f4295459be83
mak <aravind.murugaiyan@gmail.com>
2020-08-12 17:47:28 +0530

    twoNumberSum code o(n) solution

---
+++ algoexpert/java/01-easy-twoSumNumber/src/main/java/org/mak/easy/TwoNumberSum.java
@@ -0,0 +1,24 @@
+package org.mak.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoNumberSum {
+    public int[] findCouplets(Integer[] inputArray, int targetSum) {
+        if (inputArray.length < 2)
+            return new int[]{};
+        Set<Integer> lookup = new HashSet<>();
+        for (int num : inputArray){
+            final Integer potentialMatch = targetSum - num;
+            if (lookup.contains(potentialMatch))
+                return  new int[]{potentialMatch,num};
+            else
+                lookup.add(num);
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        //System.out.println(new TwoNumberSum().findCouplets());
+    }
+}
